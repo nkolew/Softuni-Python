02d36d19edb1a09369fbb3e57ff3249c58243c5c
Nikolay Kolev <n.kolew@gmail.com>
2021-05-12 16:51:29 +0300

    Solutions

---
+++ python-advanced/02 - Tuples and Sets - Lab/01. Count Same Values.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from typing import DefaultDict, List
+
+
+def get_occurences(nums: List[float]) -> dict:
+    occurences: DefaultDict = defaultdict(int)
+    for num in nums:
+        occurences[num] += 1
+    return occurences
+
+
+def fmt_occurences(ocurrences: dict):
+    result = []
+    nl = '\n'
+    for num, count in ocurrences.items():
+        result.append(f'{num} - {count} times')
+    return nl.join(result)
+
+
+def main():
+    numbers = list(map(float, input().split()))
+    occurences = get_occurences(numbers)
+    print(fmt_occurences(occurences))
+
+
+main()
---
+++ python-advanced/02 - Tuples and Sets - Lab/02. Average Student Grades.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+from typing import DefaultDict, Dict, List
+
+
+def get_n_items(n: int) -> list:
+    return [input() for _ in range(n)]
+
+
+def parse_items_to_dict(items: List[str]) -> Dict[str, List[float]]:
+    grades: DefaultDict = defaultdict(list)
+    for item in items:
+        name, grade = item.split()
+        grades[name].append(float(grade))
+    return grades
+
+
+def get_avg_grade(grade_list: List[float]) -> float:
+    return sum(grade_list) / len(grade_list)
+
+
+def fmt_output(grades: dict) -> str:
+    result = []
+    nl = '\n'
+    for name, grade_list in grades.items():
+        grades_str = ' '.join(map(lambda x: format(x, '.2f'), grade_list))
+        avg_grade = get_avg_grade(grade_list)
+        result.append(f'{name} -> {grades_str} (avg: {avg_grade:.2f})')
+    return nl.join(result)
+
+
+def main():
+    n = int(input())
+    data = get_n_items(n)
+    grades = parse_items_to_dict(data)
+    print(fmt_output(grades))
+
+
+main()
