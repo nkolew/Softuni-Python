a01acf8fa53770cd6a6213235b794dba8b9e300e
Nikolay Kolev <n.kolew@gmail.com>
2021-05-12 17:01:52 +0300

    Solution

---
+++ python-advanced/02 - Tuples and Sets - Lab/03. Record Unique Names.py
@@ -0,0 +1,21 @@
+def get_n_items(n: int) -> list:
+    return [input() for _ in range(n)]
+
+
+def get_unique_items(items: list) -> set:
+    return set(items)
+
+
+def fmt_output(items: set) -> str:
+    nl = '\n'
+    return nl.join([i for i in items])
+
+
+def main():
+    n = int(input())
+    names = get_n_items(n)
+    unique_names = get_unique_items(names)
+    print(fmt_output(unique_names))
+
+
+main()
