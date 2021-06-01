2a0fc52d160ec8b9b5ddc03b7a4ac9b8791e617c
Nikolay Kolev <n.kolew@gmail.com>
2021-06-01 17:00:26 +0300

    refactor

--- python-advanced/03 - Multidimensional Lists - Exercise/06. Knight Game_oop.py
+++ python-advanced/03 - Multidimensional Lists - Exercise/06. Knight Game_oop.py
@@ -7,15 +7,16 @@ class ChessBoard:
         self.__board = [list(input()) for _ in range(int(input()))]
         self.size = len(self.__board)
 
+    def validate_positions(self, x, y) -> bool:
+        return 0 <= x < self.size and 0 <= y < self.size
+
     @property
     def knights(self) -> List[Tuple[int, int]]:
         return [(i, j) for i in range(self.size)
                 for j in range(self.size) if self.__board[i][j] == 'K']
 
-    def valid_positions(self, x, y) -> bool:
-        return 0 <= x < self.size and 0 <= y < self.size
-
-    def get_knights_histo(self) -> DefaultDict[Tuple[int, int], int]:
+    @property
+    def knights_histo(self) -> DefaultDict[Tuple[int, int], int]:
         TARGET_DELTAS = [
             (-2, -1),
             (-1, -2),
@@ -27,16 +28,16 @@ class ChessBoard:
             (-2, 1),
         ]
 
-        knights_histo = defaultdict(int)
+        histo = defaultdict(int)
         for i in range(len(self.knights)):
             x, y = self.knights[i][0], self.knights[i][1]
             for j in range(len(TARGET_DELTAS)):
                 delta_x, delta_y = TARGET_DELTAS[j][0], TARGET_DELTAS[j][1]
                 pos_x, pos_y = x + delta_x, y + delta_y
-                if self.valid_positions(pos_x, pos_y):
+                if self.validate_positions(pos_x, pos_y):
                     if self.__board[pos_x][pos_y] == 'K':
-                        knights_histo[(x, y)] += 1
-        return knights_histo
+                        histo[(x, y)] += 1
+        return histo
 
     def remove_knight(self, coord: Tuple[int, int]) -> None:
         x, y = coord
@@ -45,13 +46,11 @@ class ChessBoard:
     def iterate_over_knights(self) -> int:
         removed_knights = 0
         while True:
-            knigthts_histo = self.get_knights_histo()
-            if not knigthts_histo:
+            if not self.knights_histo:
                 return removed_knights
-            knights = [k for k, _ in sorted(
-                knigthts_histo.items(),
-                key=lambda x: -x[1])]
-            best_knight = knights[0]
+            best_knight = [k for k, _ in sorted(
+                self.knights_histo.items(),
+                key=lambda x: -x[1])][0]
             self.remove_knight(best_knight)
             removed_knights += 1
 
